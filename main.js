30dbaa84e3b6d245c5762e1d3859225c6e891dca
felipe limeira <limeira.felipe94@gmail.com>
2025-05-11 10:43:05 -0300

    commit 4

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const map = new maplibregl.Map({
 
 // Adiciona controles de navegação (zoom in/out)
 map.addControl(new maplibregl.NavigationControl());
-// test commit 3
+// test commit 4
 // Carrega o GeoJSON dos países da América do Sul
 map.on('load', () => {
     // Adiciona a fonte GeoJSON
